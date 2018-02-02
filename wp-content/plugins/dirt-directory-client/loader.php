62998abcdff9b96ff078e03541042608f5733cf0
amandafrench <amanda@amandafrench.net>
2018-02-02 12:24:38 -0500

    Installs DiRT Directory client

---
+++ wp-content/plugins/dirt-directory-client/loader.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+Plugin Name: DiRT Directory Client
+Version: 1.2.0
+Description: Interface with the DiRT Directory API http://dirt.projectbamboo.org
+Author: Boone B Gorges
+Author URI: http://boone.gorg.es
+Text Domain: dirt-directory-client
+Domain Path: /languages
+*/
+
+define( 'DDC_VERSION', '1.2.0' );
+define( 'DDC_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+define( 'DDC_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+
+// Don't change these constants unless you know what you're doing.
+if ( ! defined( 'DDC_ENDPOINT_BASE' ) ) {
+	define( 'DDC_ENDPOINT_BASE', 'http://dirtdirectory.org/' );
+}
+
+if ( ! defined( 'DDC_ENDPOINT_URL' ) ) {
+	define( 'DDC_ENDPOINT_URL', DDC_ENDPOINT_BASE . 'services/' );
+}
+
+if ( ! defined( 'DDC_IMAGE_BASE' ) ) {
+	define( 'DDC_IMAGE_BASE', DDC_ENDPOINT_BASE . 'sites/dirtdirectory.org/files/' );
+}
+
+require DDC_PLUGIN_DIR . 'includes/template.php';
+require DDC_PLUGIN_DIR . 'includes/functions.php';
+
+if ( is_admin() ) {
+	require DDC_PLUGIN_DIR . 'includes/admin.php';
+}
+
+/**
+ * Load plugin files.
+ *
+ * @since 1.0.0
+ */
+function ddc_include() {
+	require DDC_PLUGIN_DIR . 'includes/dirt-directory-client.php';
+	require DDC_PLUGIN_DIR . 'includes/bp-integration.php';
+}
+add_action( 'bp_include', 'ddc_include' );
+
+/**
+ * Load plugin translations.
+ *
+ * @since 1.0.2
+ */
+function ddc_load_textdomain() {
+	load_plugin_textdomain( 'dirt-directory-client', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+}
+add_action( 'plugins_loaded', 'ddc_load_textdomain' );
